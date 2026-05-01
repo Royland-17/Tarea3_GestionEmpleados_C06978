ada66f3ef9e821e6f8a796592b9aa0ba37e2a28f
Royland <royland170501@gmail.com>
2026-05-01 15:26:15 -0600

    Creación y configuración de AppDbContext y creación de la carpeta Data

---
+++ Tarea3_GestionEmpleados_C06978/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea3_GestionEmpleados_C06978.Models;
+
+namespace Tarea3_GestionEmpleados_C06978.Data
+{ 
+   
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Esta propiedad crea la tabla "Empleados" basada en el modelo
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
